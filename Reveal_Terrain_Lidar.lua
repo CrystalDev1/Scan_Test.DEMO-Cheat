ff425e80f4bd754445681db71ae1d759936ddab7
CrystalDev1 <99578334+CrystalDev1@users.noreply.github.com>
2022-07-17 23:59:41 -0500

    Update Reveal_Terrain_Lidar.lua
    Now reveals barrier hitboxes

--- Reveal_Terrain_Lidar.lua
+++ Reveal_Terrain_Lidar.lua
@@ -1,8 +1,20 @@
 --- Reveals most terrain upon execution ---
 --- Crystalduck#9664 ---
-local map = game:GetService("Workspace").Map
-for i,v in pairs(map:GetChildren()) do 
-    if v.ClassName ~= 'Model' then 
-        v.Transparency = 0
+local transpar = 0.7
+
+local work = game:GetService("Workspace")
+for i,v in pairs(work.Map:GetChildren()) do 
+    if v.ClassName == 'Model' then 
+        for i1,v1 in pairs(v:GetChildren()) do
+            if v1.ClassName == 'Part' then 
+                v1.Transparency = transpar
+            end
+        end
+    else
+        v.Transparency = transpar
     end
+end
+
+for i,v in pairs(work.Barriers:GetChildren()) do 
+    v.Transparency = transpar
 end
